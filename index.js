572f624e0bd66599a0d77da7e96b9c97fd8eb31b
Gabriel Retzlaff <93136568+R3TZgl@users.noreply.github.com>
2024-07-01 21:17:52 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser')
 
 const { engine } = require('express-handlebars')
 const path = require('path')
 
+app.use(bodyparser.urlencoded({extended:false}))
 app.set('view engine', 'handlebars')
 app.engine('handlebars', engine())
 
@@ -11,40 +13,64 @@ app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
-const dadosFalsos = [
-    {
-    id: 1,
-    nome: "Gabriel Retzlaff",
-    endereco: "Estrada Morro do Meio",
-    telefone: "(47)98435-7555",
-    dataNasc: "02/12/2004"
-    },
-    {
-    id: 2,
-    nome: "Lucas Giugno",
-    endereco: "XV de Novembro",
-    telefone: "(47)93115-1341",
-    dataNasc: "12/01/2006"
-    },
-]
+const dadosfalsos = [
+  {
+    id: 1, 
+    nome: "Juh",
+    endereco: "Rua Bla Bla Bla",
+    telefone: "4002-8922",
+    datanascimento: "04/10/2004"
+  },
+  {
+    id: 2, 
+    nome: "Mariazinha",
+    endereco: "Outra rua",
+    telefone: "5555-5555",
+    datanascimento: "04/03/2024"
+  }
+] 
 
-//let count = 0
+// let contador = 0
 
 app.get('/', (req,res) =>{
-    //count += 1
-    //res.send("EU NÃO ACREDITO" + count)
-    res.render('index')
+   //contador = contador + 1
+  //res.send("EU NAO ACREDITO!!!!" + contador)
+  res.render('index')
+})
+
+app.get('/clientes', function(request, response){
+  response.render('clientes/index', {listaclientes: dadosfalsos})
+  
+})
+
+app.get('/clientes/novo', function(req,res){
+  res.render('clientes/formcliente')
+})
+
+app.post('/clientes/save', function(req,res){
+let maiorid = Math.max(...dadosfalsos.map(c => c.id))
+if (maiorid == -Infinity) maiorid = 0
+maiorid = maiorid + 1
+
+  let novocliente ={
+    id: maiorid,
+    nome: req.body.nome,
+    endereco: req.body.endereco,
+    telefone: req.body.telefone,
+    datanascimento: req.body.datanascimento
+  }
+  dadosfalsos.push(novocliente)
+  res.redirect('/clientes')
+})
+
+app.get('/clientes/alterar/:id',function(req,res){
+  let id = req.params['id']
+  let cliente = dadosfalsos.find(c => c.id == id)
+  res.render('clientes/formcliente', {cliente: cliente})
 })
 
 app.listen(3000, ()=>{
-    console.log('servidor online')
+    console.log('Servidor online')
     console.log('http://localhost:3000/')
 })
 
-app.get('/clientes', (req,res) =>{
-    res.render('clientes/index', {listaclientes: dadosFalsos})
-})
-
-app.get('/clientes/novo', (req,res) =>{
-    res.render('clientes/formCliente')
-})
